0d483bb98054a2120b883655fd19ee18597949dc
Angus Yang <googolangus@gmail.com>
2022-08-28 20:52:16 +0800

    Update for full scripts path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Construct a network
+sudo docker network create -d bridge my-network
+
+# setup db and import data sets
+scripts/db_setup.sh
+
 source ./env.test
 
 # pycat bot server
@@ -8,13 +14,8 @@ sudo docker build -t pycat .
 # Run the pybot server
 sudo docker run --network my-network -v "$PWD":/pybot22 --env-file=./env.test -d --name pycat-bot pycat
 
-echo "1. pycat running in background, you can enter the docker by\n"
-echo "  $ sudo docker exec -it pycat-bot /bin/bash\n"
-echo "2. if you would like to run in interactive mode, you can\n"
-echo "  $ sudo docker stop pycat-bot\n"
+echo "1. pycat running in background, you can enter the docker by"
+echo "  $ sudo docker exec -it pycat-bot /bin/bash"
+echo "2. if you would like to run in interactive mode, you can"
+echo "  $ sudo docker stop pycat-bot"
 echo "  $ sudo docker run -it --network my-network -v "$PWD":/pybot22 --env-file=./env.test pycat /bin/bash"
-
-# sudo docker run -it --network my-network -v "$PWD":/pybot22 --env-file=./env.test pycat /bin/bash
-
-# sudo docker exec -it pycat-bot /bin/bash
-# sudo docker commit -m "your comment" <container_id> <repository:tag>
